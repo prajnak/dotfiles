4ef1a6995d4b4f323c1617bf866cc4ee4835261a
Prajna Kandarpa <prajna.kandarpa@uwaterloo.ca>
2016-06-15 21:59:24 -0400

    first commit. add dotfiles

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/pulsar/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+alias lastcharge="syslog | grep magsafeStateChanged | tail -n 1"
+alias how2p="how2 -l python"
+alias how2mat="how2 -l matlab"
+alias how2r="how2 -l r"
+
